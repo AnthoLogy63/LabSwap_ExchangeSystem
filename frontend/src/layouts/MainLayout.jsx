a08c4ea8d06ebd7b1c868c6464d3861f12f9b7b8
FernandoGarambelM <fgarambel@unsa.edu.pe>
2025-05-17 22:59:48 -0500

    Se modifico Navbar y MainLayout para admin y user

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,13 +1,19 @@
 import Navbar from "./Navbar";
 import UserBar from "./UserBar";
+import { useAuth } from "../context/authContext";
+import { Navigate } from "react-router-dom";
 
-const MainLayout = ({ user }) => {
+const MainLayout = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) return <Navigate to="/" />;
+
   return (
     <div className="flex min-h-screen">
       <Navbar />
       <div className="flex-1 flex flex-col bg-gray-100">
         <UserBar />
-        <div className="p-6">{user}</div>
+        <div className="p-6">{children}</div>
       </div>
     </div>
   );
--- frontend/src/layouts/Navbar.jsx
+++ frontend/src/layouts/Navbar.jsx
@@ -1,60 +1,91 @@
 import { Link, useLocation } from 'react-router-dom'
-import { Home, BookOpen, GraduationCap } from 'lucide-react'  // o el ícono que uses
+import { Home, BookOpen, GraduationCap, ClipboardList, History} from 'lucide-react'  // o el ícono que uses
+import { useAuth } from "../context/authContext";
 
 const Navbar = () => {
   const location = useLocation()
+  const { userRole } = useAuth();
 
   const isActive = (path) => location.pathname === path
 
   return (
     <nav className="bg-[#0E6F79] text-white w-60 min-h-screen p-4">
       <h1 className="text-3xl font-bold mb-10 tracking-wide">LAB SWAP</h1>
-
       <ul className="flex flex-col gap-3">
-        <li>
-          <Link
-            to="/inicio"
-            className={`flex items-center gap-3 px-4 py-2 rounded-xl transition-colors ${
-              isActive('/inicio') ? 'bg-white text-[#0E6F79] font-semibold' : 'hover:bg-[#0C5E6A]'
-            }`}
-          >
-            <Home
-              size={20}
-              className={isActive('/inicio') ? 'fill-[#0E6F79]' : ''}
-            />
-            Inicio
-          </Link>
-        </li>
+        {userRole === "user" && (
+          <>
+            <li>
+              <Link
+                to="/inicio"
+                className={`flex items-center gap-3 px-4 py-2 rounded-xl transition-colors ${
+                  isActive("/inicio")
+                    ? "bg-white text-[#0E6F79] font-semibold"
+                    : "hover:bg-[#0C5E6A]"
+                }`}
+              >
+                <Home size={20} />
+                Inicio
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/cursos"
+                className={`flex items-center gap-3 px-4 py-2 rounded-xl transition-colors ${
+                  isActive("/cursos")
+                    ? "bg-white text-[#0E6F79] font-semibold"
+                    : "hover:bg-[#0C5E6A]"
+                }`}
+              >
+                <BookOpen size={20} />
+                Lista de Cursos
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/mis-cursos"
+                className={`flex items-center gap-3 px-4 py-2 rounded-xl transition-colors ${
+                  isActive("/mis-cursos")
+                    ? "bg-white text-[#0E6F79] font-semibold"
+                    : "hover:bg-[#0C5E6A]"
+                }`}
+              >
+                <GraduationCap size={20} />
+                Mis Cursos
+              </Link>
+            </li>
+          </>
+        )}
 
-        <li>
-          <Link
-            to="/cursos"
-            className={`flex items-center gap-3 px-4 py-2 rounded-xl transition-colors ${
-              isActive('/cursos') ? 'bg-white text-[#0E6F79] font-semibold' : 'hover:bg-[#0C5E6A]'
-            }`}
-          >
-            <BookOpen
-              size={20}
-              className={isActive('/cursos') ? 'fill-[#0E6F79]' : ''}
-            />
-            Lista de Cursos
-          </Link>
-        </li>
-
-        <li>
-          <Link
-            to="/mis-cursos"
-            className={`flex items-center gap-3 px-4 py-2 rounded-xl transition-colors ${
-              isActive('/mis-cursos') ? 'bg-white text-[#0E6F79] font-semibold' : 'hover:bg-[#0C5E6A]'
-            }`}
-          >
-            <GraduationCap
-              size={20}
-              className={isActive('/mis-cursos') ? 'fill-[#0E6F79]' : ''}
-            />
-            Mis Cursos
-          </Link>
-        </li>
+        {userRole === "admin" && (
+          <>
+            <li>
+              <Link
+                to="/admin/panel-cursos"
+                className={`flex items-center gap-3 px-4 py-2 rounded-xl transition-colors ${
+                  isActive("/admin/panel-cursos")
+                    ? "bg-white text-[#0E6F79] font-semibold"
+                    : "hover:bg-[#0C5E6A]"
+                }`}
+              >
+                <ClipboardList size={20} />
+                Panel de Cursos
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/admin/historial"
+                className={`flex items-center gap-3 px-4 py-2 rounded-xl transition-colors ${
+                  isActive("/admin/historial")
+                    ? "bg-white text-[#0E6F79] font-semibold"
+                    : "hover:bg-[#0C5E6A]"
+                }`}
+              >
+                <History size={20} />
+                Historial
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   )
