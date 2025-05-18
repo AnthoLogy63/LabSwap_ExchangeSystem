42f1f055e71370537249806d3899450bd19eef07
FernandoGarambelM <fgarambel@unsa.edu.pe>
2025-05-17 23:00:49 -0500

    Se modifico app para poner todas las rutas en navbar para user y admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,85 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Navbar from "./layouts/Navbar"
-import UserBar from "./layouts/UserBar"
-import InicioDeSesion from "./pages/USER/InicioDeSesion"
+import MainLayout from './layouts/MainLayout'
+
+import InicioDeSesion from "./pages/InicioDeSesion"
 import PaginaDeInicio from "./pages/USER/PaginaDeInicio"
 import PaginaDeCursos from './pages/USER/PaginaDeCursos';
+import EdicionCursosEstudiante from './pages/USER/EdicionCursosEstudiante'
+import ContactarIntercambio from './pages/USER/ContactarIntercambio'
+import PerfilDelEstudiante from './pages/USER/PerfilDelEstudiante'
+
+import PaneldeCursos_Admin from './pages/ADMIN/PaneldeCursos_Admin'
+import PanelHistorial_Admin from './pages/ADMIN/PanelHistorial-Admin'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex min-h-screen">
-        <Navbar />
-        <div className="flex-1 flex flex-col">
-          <UserBar />
-          <div className="p-6 flex-1">
-            <Routes>
-              <Route path="/" element={<InicioDeSesion />} />
-              <Route path="/inicio" element={<PaginaDeInicio />} />
-              <Route path="/cursos" element={<PaginaDeCursos />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<InicioDeSesion />} />
+
+        {/* Rutas protegidas envueltas en MainLayout */}
+        <Route
+          path="/inicio"
+          element={
+            <MainLayout>
+              <PaginaDeInicio />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/cursos"
+          element={
+            <MainLayout>
+              <PaginaDeCursos />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/mis-cursos"
+          element={
+            <MainLayout>
+              <EdicionCursosEstudiante />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/perfil"
+          element={
+            <MainLayout>
+              <PerfilDelEstudiante />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/intercambio"
+          element={
+            <MainLayout>
+              <ContactarIntercambio />
+            </MainLayout>
+          }
+        />
+
+        {/* ADMIN */}
+        <Route
+          path="/admin/panel-cursos"
+          element={
+            <MainLayout>
+              <PaneldeCursos_Admin />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/admin/historial"
+          element={
+            <MainLayout>
+              <PanelHistorial_Admin />
+            </MainLayout>
+          }
+        />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
