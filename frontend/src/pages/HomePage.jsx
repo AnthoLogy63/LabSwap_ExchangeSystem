cf6372438c5a081037c37b1a103e1c05218f2dc4
VelmorK <velmork313yt@gmail.com>
2025-05-26 16:59:52 -0500

    Modificacion tamaño de letra, modificacion de imagen

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 // src/pages/PaginaDeInicio.jsx
-import publicaImg from '../assets/PaginaDeInicio/padoru.jpg';
-import buscaImg from '../assets/PaginaDeInicio/padoru.jpg';
-import cambiaImg from '../assets/PaginaDeInicio/padoru.jpg';
+import publicaImg from '../assets/PaginaDeInicio/generic.jpg';
+import buscaImg from '../assets/PaginaDeInicio/generic.jpg';
+import cambiaImg from '../assets/PaginaDeInicio/generic.jpg';
 
 const HomePage = () => {
   const pasos = [
@@ -11,15 +11,15 @@ const HomePage = () => {
   ];
 
   return (
-    <div className="text-center">
-      <h1 className="text-4xl font-bold mb-4">Bienvenido a Lab-Swap</h1>
-      <p className="mb-8">Una página para intercambiar los cursos que necesitas</p>
+    <div className="text-left ml-20">
+      <h1 className="text-8xl font-bold mb-8">Bienvenido a Lab-Swap</h1>
+      <p className="text-3xl mb-12">Una página para intercambiar los cursos que necesitas</p>
 
       <h2 className="text-2xl font-bold mb-6">Solo con 3 pasos:</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         {pasos.map((paso, idx) => (
           <div key={idx} className="flex flex-col items-center">
-            <img src={paso.img} alt={paso.title} className="mb-3 w-16 h-16" />
+            <img src={paso.img} alt={paso.title} className="mb-3 w-86" />
             <h3 className="font-bold">{paso.title}</h3>
             <p className="text-sm text-slate-600 max-w-xs">{paso.desc}</p>
           </div>
