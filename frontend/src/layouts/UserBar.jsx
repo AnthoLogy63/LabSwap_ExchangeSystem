5868263ca4bf304d46aa9fa84380a6c37ca4ea64
Choflis <lluquecon@unsa.edu.pe>
2025-06-02 16:50:54 -0500

    barra de perfil ya pone el nombre según tu nombre de google

--- frontend/src/layouts/UserBar.jsx
+++ frontend/src/layouts/UserBar.jsx
@@ -1,21 +1,22 @@
-import { Link } from 'react-router-dom'
-import { UserCircle2 } from 'lucide-react'
+import { Link } from 'react-router-dom';
+import { Pencil, UserCircle2 } from 'lucide-react';
+import { useAuth } from '../../src/context/AuthContext';
 
 function UserBar() {
-  const user = {
-    nombre: 'Pepe Carrillo',
-  }
+  const { user } = useAuth();
 
   return (
-    <div className="bg-[] flex justify-end items-center p-6">
+    <div className="flex justify-end items-center p-6">
       <Link to="/perfil" className="flex items-center space-x-2 hover:underline hover:text-red-700 transition">
         <div className="bg-[#761A11] p-1 rounded-full">
-              <UserCircle2 className="text-white w-8 h-8" />
-            </div>
-        <span className="text-xl text-red-900 font-medium pr-10 pl-2">{user.nombre}</span>
+          <UserCircle2 className="text-white w-8 h-8" />
+        </div>
+        <span className="text-xl text-red-900 font-medium pr-10 pl-2">
+          {user?.name || 'Invitado'}
+        </span>
       </Link>
     </div>
-  )
+  );
 }
 
-export default UserBar
+export default UserBar;
