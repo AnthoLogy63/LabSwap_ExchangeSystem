ff1874f2d30c453e8d7149bfe216424d357ca1bc
WilliamLawrence25 <wchoquehuancab@unsa.edu.pe>
2025-06-09 14:37:25 -0500

    ConfirmDeteleModal terminado aspecto y funcionalidad

--- frontend/src/components/ConfirmDeleteModal.jsx
+++ frontend/src/components/ConfirmDeleteModal.jsx
@@ -5,20 +5,20 @@ const ConfirmDeleteModal = ({ isOpen, onCancel, onConfirm }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded-lg shadow-md w-[300px]">
-        <h2 className="text-xl font-bold mb-4">¿Estás seguro?</h2>
-        <p className="mb-6">¿Deseas eliminar este curso ofrecido?</p>
-        <div className="flex justify-end gap-4">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 border-1">
+      <div className="bg-white w-full max-w-3xl p-16 rounded-3xl shadow-xl border-4 border-teal-800 text-teal-900">
+        <h2 className="text-5xl font-bold mb-8 text-center">Confirmacion para borrar<br></br> oferta de curso</h2>
+        <p className="text-2xl mb-10 text-center">Estas por borrar tu oferta del curso,<br></br> necesitas confirmar la acción.</p>
+        <div className="flex justify-center gap-20 text-xl">
           <button
             onClick={onCancel}
-            className="bg-gray-300 text-gray-800 px-4 py-2 rounded hover:bg-gray-400"
+            className=" bg-teal-600 hover:bg-teal-700 text-white px-7 py-2 rounded-md"
           >
             Cancelar
           </button>
           <button
             onClick={onConfirm}
-            className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
+            className="bg-red-600 text-white hover:bg-red-700 px-7 py-2 rounded-md"
           >
             Confirmar
           </button>
