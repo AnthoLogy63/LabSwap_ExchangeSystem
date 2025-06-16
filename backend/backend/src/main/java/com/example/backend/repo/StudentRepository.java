074c40a3b81b7715d9452c7e3b19b10d05777d3d
FernandoGarambelM <fgarambel@unsa.edu.pe>
2025-06-16 01:03:11 -0500

    Nueva defenicion para student

--- backend/backend/src/main/java/com/example/backend/repo/StudentRepository.java
+++ backend/backend/src/main/java/com/example/backend/repo/StudentRepository.java
@@ -2,6 +2,8 @@ package com.example.backend.repo;
 
 import com.example.backend.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
+    Optional<Student> findByStudentEmail(String studentEmail);
 }
